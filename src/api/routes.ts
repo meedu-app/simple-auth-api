77f5774cefc329ebb971de16515ea2a3ab89d884
darwin-morocho <darwin.morocho@icloud.com>
2023-01-19 19:56:44 -0500

    first commit

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -2,6 +2,7 @@ import { Express } from "express";
 import { signIn, signUp } from "../controllers/sign-in-controller";
 
 export const api = (app: Express) => {
+  app.get("/", (req, res) => res.send('Wellcome'));
   app.post("/api/sign-in", signIn);
   app.post("/api/sign-up", signUp);
 };
